62d8dc1a5508600c1294c53595c91b20056b3ce8
moacirdavidag <moacirdavidag@gmail.com>
2023-09-03 18:07:46 -0300

    Envolvendo consultas ao banco de dados sobre campeonatos com blocos try...catch

--- src/controllers/campeonatoController.js
+++ src/controllers/campeonatoController.js
@@ -1,33 +1,37 @@
 import { prismaClient } from "../database/prisma-client-js.js";
 
 
-    export const getCampeonatos = async (request, response) =>{
+export const getCampeonatos = async (request, response) => {
+    try {
         const campeonatos = await prismaClient.campeonato.findMany();
         response.status(200).json(campeonatos);
+    } catch (e) {
+        return response.status(500).send(e.message);
     }
+}
 
-    export const createCampeonato = async (request, response) =>{
-        try {
-            const { nome, data_inicio, data_fim } = request.body;
+export const createCampeonato = async (request, response) => {
+    try {
+        const { nome, data_inicio, data_fim } = request.body;
 
-            const campeonato = await prismaClient.campeonato.create({
-                data: {
-                    nome,
-                    data_inicio,
-                    data_fim,
-                },
-            });
+        const campeonato = await prismaClient.campeonato.create({
+            data: {
+                nome,
+                data_inicio,
+                data_fim,
+            },
+        });
 
-            return response.status(201).json(campeonato);
-        }
-        catch (error) {
-            return response.status(500).json({ error: 'An error occurred' });
-        }
+        return response.status(201).json(campeonato);
     }
+    catch (error) {
+        return response.status(500).send(e.message);
+    }
+}
 
-    export const updateCampeonato = async (request, response) => {
-    
-        const campeonato = await prismaClient.campeonato.findUnique({ where: {  id: request.params.id } });
+export const updateCampeonato = async (request, response) => {
+    try {
+        const campeonato = await prismaClient.campeonato.findUnique({ where: { id: request.params.id } });
         if (!campeonato) {
             return response.status(404).json({ err: 'Não achei o Campeonato' });
         }
@@ -38,32 +42,29 @@ import { prismaClient } from "../database/prisma-client-js.js";
             data: request.body
         });
         return response.status(200).json(updatedCampeonato)
-    
+    } catch (e) {
+        return response.status(500).send(e.message);
     }
+}
 
 
-
- 
-
-    export const deleteCampeonato = async (request, response) => {
+export const deleteCampeonato = async (request, response) => {
     try {
         const campeonato = await prismaClient.campeonato.findUnique({ where: { id: request.params.id } });
         if (!campeonato) {
             return response.status(404).json({ err: 'Campeonato não encontrado!' });
         }
-    
+
         const deletedCampeonato = await prismaClient.campeonato.delete({
             where: {
                 id: request.params.id
             }
         })
-      return response.json(deletedCampeonato);
-      
+        return response.status(200).json(deletedCampeonato);
+
     } catch (error) {
-      return response.status(500).json({ error: 'An error occurred' });
+        return response.status(500).json(error.message);
     }
-  }
+}
 
-
-
-export default { getCampeonatos, createCampeonato, updateCampeonato , deleteCampeonato};
+export default { getCampeonatos, createCampeonato, updateCampeonato, deleteCampeonato };
