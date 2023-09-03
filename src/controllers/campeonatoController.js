2582d377aa34dd5c29641bfd6325bca5c9dbcb77
JoaoAlmir <junivion@gmail.com>
2023-09-03 19:40:32 -0300

    add get por id e comentarios

--- src/controllers/campeonatoController.js
+++ src/controllers/campeonatoController.js
@@ -10,6 +10,18 @@ export const getCampeonatos = async (request, response) => {
     }
 }
 
+export const getCampeonato = async (request, response) => {
+    try {
+        let idParams = request.params.id;
+        const campeonato = await prismaClient.campeonato.findUnique({
+            where: { id:idParams },
+          })
+        response.status(200).json(campeonato);
+    } catch (e) {
+        return response.status(500).send(e.message);
+    }
+}
+
 export const createCampeonato = async (request, response) => {
     try {
         const { nome, data_inicio, data_fim } = request.body;
