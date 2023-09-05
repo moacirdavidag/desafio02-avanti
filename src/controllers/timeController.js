39023d538d5bf87f33833dc1d95c3c63e9ca602d
deybson-stack <118633158+deybson-stack@users.noreply.github.com>
2023-09-04 21:53:38 -0300

    Update timeController.js
    atualização do timeController

--- src/controllers/timeController.js
+++ src/controllers/timeController.js
@@ -1,75 +1,70 @@
-
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-
-const router = express.Router();
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
 
 // Listar todos os times
-router.get('/times', async (req, res) => {
+export const getTimes = async (request, response) => {
   try {
     const times = await prisma.time.findMany();
-    res.json(times);
+    response.json(times);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Erro ao buscar times' });
+    response.status(500).json({ error: 'Erro ao buscar times' });
   }
-});
+};
 
 // Obter detalhes de um time por ID
-router.get('/time/:id', async (req, res) => {
-  const { id } = req.params;
+export const getTime = async (request, response) => {
+  const { id } = request.params;
   try {
     const time = await prisma.time.findUnique({ where: { id: parseInt(id) } });
     if (!time) {
-      res.status(404).json({ error: 'Time não encontrado' });
+      response.status(404).json({ error: 'Time não encontrado' });
     } else {
-      res.json(time);
+      response.json(time);
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Erro ao buscar o time' });
+    response.status(500).json({ error: 'Erro ao buscar o time' });
   }
-});
+};
 
 // Criar um novo time
-router.post('/times', async (req, res) => {
-  const { nome, fundacao } = req.body;
+export const createTime = async (request, response) => {
+  const { nome, fundacao } = request.body;
   try {
     const time = await prisma.time.create({ data: { nome, fundacao } });
-    res.status(201).json(time);
+    response.status(201).json(time);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Erro ao criar time' });
+    response.status(500).json({ error: 'Erro ao criar time' });
   }
-});
+};
 
 // Atualizar dados de um time por ID
-router.put('/times/:id', async (req, res) => {
-  const { id } = req.params;
-  const { nome, fundacao } = req.body;
+export const updateTime = async (request, response) => {
+  const { id } = request.params;
+  const { nome, fundacao } = request.body;
   try {
     const updatedTime = await prisma.time.update({
       where: { id: parseInt(id) },
       data: { nome, fundacao },
     });
-    res.json(updatedTime);
+    response.json(updatedTime);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Erro ao atualizar o time' });
+    response.status(500).json({ error: 'Erro ao atualizar o time' });
   }
-});
+};
 
 // Excluir um time por ID
-router.delete('/times/:id', async (req, res) => {
-  const { id } = req.params;
+export const deleteTime = async (request, response) => {
+  const { id } = request.params;
   try {
     await prisma.time.delete({ where: { id: parseInt(id) } });
-    res.json({ message: 'Time excluído com sucesso' });
+    response.json({ message: 'Time excluído com sucesso' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Erro ao excluir o time' });
+    response.status(500).json({ error: 'Erro ao excluir o time' });
   }
-});
+};
 
-module.exports = router;
+export default { getTimes, createTime, updateTime, deleteTime };
